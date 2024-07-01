0a6ff0e3b7db03e6af46889236eacd2a77ef3d7a
gagansuryanagara <gagansuryanagara@gmail.com>
2024-07-01 22:05:03 +0700

    table master katergory, master produk, stok

--- controller/c_user.js
+++ controller/c_user.js
@@ -5,6 +5,7 @@ module.exports =
     index: async function(req,res){
         let dataview = {
             konten: "user-management/index",
+            req: req,
             uri_segment: req.path.split("/"),
             users: await m_user.get_semua_user()
         }
@@ -23,17 +24,18 @@ module.exports =
     },
 
 
-
+    
     proses_simpan:async function (req,res) {
         try{
             let insert = await m_user.tambah(req)
             let isi_notif = `behasil membuat user baru` 
             if(insert.affectedRows > 0){
-                res.redirect(`/user-management?note=${isi_notif}`)
+                res.redirect(`/user-management?note=sukses&pesan=${isi_notif}`)
             }
         } catch (error) {
             let dataview ={
                 konten  : "user-management/form-tambah",
+                req : req,
                 uri_segment: req.path.split("/"),
                 info_error  : error,
             }
