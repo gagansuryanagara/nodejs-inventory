cafb8270b116a444e73ac6b0a84c62e7f04ab4cd
gagansuryanagara <gagansuryanagara@gmail.com>
2024-07-02 20:29:25 +0700

    config database

---
+++ model/m_produk.js
@@ -0,0 +1,21 @@
+const mysql     = require('mysql2')
+const db = require("../config/database").db
+
+module.exports = 
+{
+    get_semua_produk: function () {
+        let sql = mysql.format(
+                `SELECT * from master_produk`
+        )
+
+        return new Promise ((resolve,reject)=>{
+            db.query(sql,function(errorSql,hasil){
+                if (errorSql){
+                    reject(errorSql)
+                } else {
+                    resolve(hasil)
+                }
+            })
+        })   
+    },
+}
--- model/m_user.js
+++ model/m_user.js
@@ -1,12 +1,5 @@
-const bcrypt    = require ('bcryptjs')
 const mysql     = require('mysql2')
-const db=mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'xdb_belajar_database',
-})
-db.connect()
+const db = require("../config/database").db
 
 module.exports = 
 {
